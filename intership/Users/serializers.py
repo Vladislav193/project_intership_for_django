cd8c6fd41fbfe7e50f943e5eb2401b2dcd1dbfb7
Vladislav <vladrosenrol@gmail.com>
2024-08-06 21:47:10 +0300

    костяк по отделом собран

--- intership/Users/serializers.py
+++ intership/Users/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 from .models import User, Division
 
+class DivisionSerializer(serializers.ModelSerializer):
+    """Сериализатор для заполенение отделов"""
+    class Meta:
+        model = Division
+        fields = ("name", "director")
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     """Сериализатор для users"""
+    division = DivisionSerializer()
     class Meta:
         model = User
         fields = ("name", "last_name", "login", "email",
                   "password", "division")
 
 
-class DivisionSerializer(serializers.ModelSerializer):
-    """Сериализатор для заполенение отделов"""
-    class Meta:
-        model = Division
-        fields = ("name", "director")
